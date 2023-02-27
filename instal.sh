f27c3e93b24d9c37d00bafcdb6ad33505c87e97a
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2023-02-27 19:14:04 +0200

    Clipboard fix

--- instal.sh
+++ instal.sh
@@ -5,13 +5,19 @@ sudo apt-get update
 curl -LJO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
 sudo dpkg -i ./nvim-linux64.deb
 sudo apt-get install -f
-sudo apt-get install git python3 golang-go gdu ripgrep -y
+sudo apt-get install zip git python3 golang-go gdu ripgrep -y
 # lazygit
 # bottom
 
+# Install win32yank (comment out the lines below if you are not used wsl2)
+curl -sLo /tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
+chmod +x /tmp/win32yank.exe
+sudo mv /tmp/win32yank.exe /usr/local/bin/
+
 # Copy AstroNvim config
 if [ ! -d ~/.config/nvim ]; then
-  git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+  git clone -b v2.11.8 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 fi
 
 # Copy custom config
