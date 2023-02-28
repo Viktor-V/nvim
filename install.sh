1c9c9312fb56c56134ed0d7c0d2620c240ffefd6
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2023-02-28 21:51:11 +0200

    Installed lazygit + bottom

--- install.sh
+++ install.sh
@@ -2,12 +2,22 @@
 
 # Install all required packages
 sudo apt-get update
-curl -LJO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
-sudo dpkg -i ./nvim-linux64.deb
+sudo apt-get install zip git lua5.4 python3 golang-go gdu ripgrep -y
+
+# Install neovim
+curl -sLo /tmp/nvim.deb https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
+sudo dpkg -i /tmp/nvim.deb
 sudo apt-get install -f
-sudo apt-get install zip git python3 golang-go gdu ripgrep -y
-# lazygit
-# bottom
+
+# Install bottom
+curl -sLo /tmp/bottom.deb https://github.com/ClementTsang/bottom/releases/download/0.8.0/bottom_0.8.0_amd64.deb
+sudo dpkg -i /tmp/bottom.deb
+sudo apt-get install -f
+
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -sLo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+sudo tar xf /tmp/lazygit.tar.gz -C /usr/local/bin lazygit
 
 # Install win32yank (comment out the lines below if you are not used wsl2)
 curl -sLo /tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
